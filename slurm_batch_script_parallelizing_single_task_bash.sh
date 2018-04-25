da47db6a99802384aca141265af393f4ea89d2b1
Kristina Plazonic <kplazo@gmail.com>
2018-04-25 16:56:42 -0400

    adding more info to batch script for bash single task

--- slurm_batch_script_parallelizing_single_task_bash.sh
+++ slurm_batch_script_parallelizing_single_task_bash.sh
@@ -2,12 +2,12 @@
 
 #SBATCH --job-name=kris_hello ## Replace with your jobname 
 #SBATCH --partition=main # Partition (job queue) 
-#SBATCH -N 2 # Number of nodes 
-#SBATCH -n 2 # Number of tasks (usually = cores) on each node
+#SBATCH -N 1
+#SBATCH -n 1
 #SBATCH --cpus-per-task=1 ## keep it 1 in most cases
 #SBATCH --mem=100 # Real memory per node required (MB) 
 #SBATCH --time=00:10:00 # Total run time limit (HH:MM:SS) 
-#SBATCH --array=0-5  #how many do we have? 
+#SBATCH --array=0-5  # defines an array job
 
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=kp807@oarc.rutgers.edu
@@ -18,12 +18,10 @@
 ### Declare job non-rerunable
 #SBATCH --no-requeue
 
-#filelistname = "/home/kp807/projects/aditi_data/list_of_files_for_parallel_process.txt"
-#filelist = open(filelistname, "r").read().splitlines()
-
 
 WORKING_DIRECTORY='/home/kp807/projects/aditi_data/example_data_parallelization/'  # change this to your directory
 readarray FILELIST < $WORKING_DIRECTORY/list_of_files_for_parallel_process.txt
 
-echo $WORKING_DIRECTORY/directory_to_process/${FILELIST[$SLURM_ARRAY_TASK_ID]}
+# Note: I'm passing relative path to file (e.g. "directory_to_process/cat.txt") to the bash script. 
+# This works because the bash script deals with relative paths
 /home/kp807/projects/aditi_data/single_task_script.sh directory_to_process/${FILELIST[$SLURM_ARRAY_TASK_ID]} 
