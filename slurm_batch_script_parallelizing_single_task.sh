34abb951dc6afdfd7c8e9e971eefc0edea3254fe
Kristina Plazonic <kplazo@gmail.com>
2018-04-25 12:47:01 -0400

    added more info to slurm batch job

--- slurm_batch_script_parallelizing_single_task.sh
+++ slurm_batch_script_parallelizing_single_task.sh
@@ -2,12 +2,12 @@
 
 #SBATCH --job-name=kris_hello ## Replace with your jobname 
 #SBATCH --partition=main # Partition (job queue) 
-#SBATCH -N 2 # Number of nodes 
-#SBATCH -n 2 # Number of tasks (usually = cores) on each node
+#SBATCH -N 1 # Number of nodes  - since it is an array job, choose 1 node per array index
+#SBATCH -n 1 # Number of tasks  - since it is an array job, choose 1 node per array index
 #SBATCH --cpus-per-task=1 ## keep it 1 in most cases
 #SBATCH --mem=100 # Real memory per node required (MB) 
 #SBATCH --time=00:10:00 # Total run time limit (HH:MM:SS) 
-#SBATCH --array=0-5  #how many do we have? 
+#SBATCH --array=0-5  #how many do we have?  count the files with 'wc -l list_of_files_for_parallel_process.txt'
 
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=kp807@oarc.rutgers.edu
@@ -18,12 +18,13 @@
 ### Declare job non-rerunable
 #SBATCH --no-requeue
 
-#filelistname = "/home/kp807/projects/aditi_data/list_of_files_for_parallel_process.txt"
-#filelist = open(filelistname, "r").read().splitlines()
-
 
 WORKING_DIRECTORY='/home/kp807/projects/aditi_data/example_data_parallelization/'  # change this to your directory
-readarray FILELIST < $WORKING_DIRECTORY/list_of_files_for_parallel_process.txt
+readarray FILELIST < $WORKING_DIRECTORY/list_of_files_for_parallel_process.txt     # read the list of files into variable of type array in bash, called FILELIST
 
-echo $WORKING_DIRECTORY/directory_to_process/${FILELIST[$SLURM_ARRAY_TASK_ID]}
+echo $WORKING_DIRECTORY/directory_to_process/${FILELIST[$SLURM_ARRAY_TASK_ID]}     # you can omit this command, this is for debug purposes only
+
+# NOTE: SLURM_ARRAY_TASK_ID is a slurm variable that keeps track of which task in the array job you are processing
+# NOTE: ${FILELIST[$SLURM_ARRAY_TASK_ID]}  is the SLURM_ARRAY_TASK_IDth entry in the array variable FILELIST
 /home/kp807/projects/aditi_data/single_task_script.py $WORKING_DIRECTORY/directory_to_process/${FILELIST[$SLURM_ARRAY_TASK_ID]} 
+
